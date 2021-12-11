5fb7a9d964e1b4e2903c19108495ca36b7f174ce
RikardLundberg <rikardlundberg90@gmail.com>
2021-12-11 10:53:52 +0100

    day_11 first star

---
+++ day_11/day_11.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+void increaseNeighbours(int i, int j);
+void increaseNeighbour(int i, int j);
+std::vector<std::vector<int>> matrix;
+int flashCount = 0;
+
+int main()
+{
+	int numberOfSteps = 100;
+
+	std::string input;
+	while (std::cin >> input && input != "eof")
+	{
+		std::vector<int> newRow;
+		for (int i = 0; i < input.size(); i++)
+			newRow.push_back(input[i] - 48);
+		matrix.push_back(newRow);
+	}
+
+	int currentStep = 0;
+	while (currentStep != numberOfSteps) {
+		for (int i = 0; i < matrix.size(); i++)
+		{
+			for (int j = 0; j < matrix[i].size(); j++)
+			{
+				matrix[i][j]++;
+				if (matrix[i][j] == 10)
+					increaseNeighbours(i, j);
+			}
+		}
+		for (int i = 0; i < matrix.size(); i++)
+		{
+			for (int j = 0; j < matrix[i].size(); j++)
+			{
+				if (matrix[i][j] > 9)
+				{
+					flashCount++;
+					matrix[i][j] = 0;
+				}
+			}
+		}
+
+		currentStep++;
+	}
+
+	std::cout << flashCount;
+}
+
+
+void increaseNeighbours(int i, int j)
+{
+	if (j != 0) increaseNeighbour(i, j - 1);
+	if (j != 0 && i != 0) increaseNeighbour(i - 1, j - 1);
+	if (j != 0 && i != matrix[i].size() - 1) increaseNeighbour(i + 1, j - 1);
+
+	if (j != matrix.size() - 1) increaseNeighbour(i, j + 1);
+	if (j != matrix.size() - 1 && i != 0) increaseNeighbour(i - 1, j + 1);
+	if (j != matrix.size() - 1 && i != matrix[i].size() - 1) increaseNeighbour(i + 1, j + 1);
+
+	if (i != 0) increaseNeighbour(i - 1, j);
+	if (i != matrix.size() - 1) increaseNeighbour(i + 1, j);
+}
+
+void increaseNeighbour(int i, int j)
+{
+	matrix[i][j]++;
+
+	if (matrix[i][j] == 10) {
+		//flashCount++;
+		increaseNeighbours(i, j);
+	}
+}
