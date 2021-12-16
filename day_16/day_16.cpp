3b6789fae3832bbfe8114866fa50c308f01c79f2
RikardLundberg <rikardlundberg90@gmail.com>
2021-12-16 12:04:16 +0100

    seems right day_16 but wrong answer? created day_17

--- day_16/day_16.cpp
+++ day_16/day_16.cpp
@@ -3,19 +3,21 @@
 #include <vector>
 #include <chrono>
 
-int TranslateBinary(std::string val);
+long long int TranslateBinary(std::string val);
 void parseInput();
 std::chrono::time_point<std::chrono::steady_clock> start_time;
 std::string HexaToBinary(char hexa);
-void ReadOperator();
+long long int ReadOperator(int type);
 std::string binaryInput = "";
 int totalVersion = 0;
 int counter = 0;
+std::string translation = "";
 
 int main()
 {
 	parseInput();
 
+	long long int result = 0;
 	std::string versionStr, typeStr;
 	versionStr += binaryInput.substr(0, 3);
 	int version = TranslateBinary(versionStr);
@@ -37,13 +39,50 @@ int main()
 	}
 	else
 	{
-		ReadOperator();
+		result += ReadOperator(type);
 	}
 
+	std::cout << "End result: " << result << std::endl;
 	std::cout << "Total Version: " << totalVersion << std::endl;
+	std::cout << std::endl << std::endl << translation;
 }
 
-void ReadOperator()
+void LogType(int type)
+{
+	switch (type)
+	{
+	case 0:
+		translation += "\nSUM(";
+		break;
+	case 1:
+		translation += "\nMUL(";
+		break;
+	case 2:
+		translation += "\nMIN(";
+		break;
+	case 3:
+		translation += "\nMAX(";
+		break;
+	case 5:
+		translation += "\nGRT(";
+		break;
+	case 6:
+		translation += "\nLES(";
+		break;
+	case 7:
+		translation += "\nEQU(";
+		break;
+	}
+}
+
+
+bool CanFitMore(int counter, int subPackEnd) 
+{
+	return (counter < subPackEnd);
+}
+
+
+long long int ReadOperator(int type)
 {
 	bool isFifteen = false; // else eleven
 	if (binaryInput[counter] == '0')
@@ -59,6 +98,10 @@ void ReadOperator()
 	int L = TranslateBinary(Lstr);
 	int subPackSize = L + counter;
 	bool morePackages = isFifteen || L > 0;
+	std::vector<long long int> values;
+
+	LogType(type);
+
 	while (morePackages)
 	{
 		for (int sub = 0; sub < L; sub++)
@@ -67,28 +110,80 @@ void ReadOperator()
 			counter += 3;
 			int type = TranslateBinary(binaryInput.substr(counter, 3));
 			counter += 3;
+			if (isFifteen && !CanFitMore(counter, subPackSize))
+				break;
 			if (type != 4)
-				ReadOperator();
+			{
+				values.push_back(ReadOperator(type));
+			}
 			else
 			{
 				bool lastGroup = false;
+				std::string totalVal = "";
+				if (isFifteen && !CanFitMore(counter, subPackSize))
+					break;
 				while (!lastGroup)
 				{
 					std::string subGroup;
 					subGroup += binaryInput.substr(counter, 5);
+					totalVal += binaryInput.substr(counter + 1, 4);
 					counter += 5;
 					if (subGroup[0] == '0')
 						lastGroup = true;
 				}
+				long long int newValue = TranslateBinary(totalVal);
+				translation += std::to_string(newValue) + ", ";
+				values.push_back(newValue);
 			}
-			if (isFifteen) {
-				morePackages = (counter < subPackSize);
+			if (isFifteen && !CanFitMore(counter, subPackSize)) {
+				morePackages = false;//(counter + 10 < subPackSize);
 				break;
 			}
 		}
 		if (!isFifteen)
 			morePackages = false;
 	}
+	if (isFifteen)
+		counter = subPackSize;
+
+	if(translation.substr(translation.size() - 2, 2) == ", ")
+		translation = translation.substr(0, translation.size() - 2);
+	translation += ")";
+
+	if (0 == type) {
+		long long int start = 0;
+		for (int i = 0; i < values.size(); i++)
+			start += values[i];
+		return start;
+	}
+	else if (1 == type) {
+		long long int start = values[0];
+		for (int i = 1; i < values.size(); i++)
+			start *= values[i];
+		return start;
+	}
+	else if (2 == type) {
+		long long int min = INT_MAX;
+		for (int val : values)
+			if (val < min)
+				min = val;
+		return min;
+	}
+	else if (3 == type) {
+		long long int max = INT_MIN;
+		for (int val : values)
+			if (val > max)
+				max = val;
+		return max;
+	}
+	else if (5 == type) 
+		return (values[0] > values[1]);
+	else if (6 == type)
+		return (values[0] < values[1]);
+	else if (7 == type)
+		return (values[0] == values[1]);
+
+	throw;
 }
 
 std::string HexaToBinary(char hexa)
@@ -145,10 +240,10 @@ std::string HexaToBinary(char hexa)
 	}
 }
 
-int TranslateBinary(std::string val)
+long long int TranslateBinary(std::string val)
 {
-	int retVal = 0;
-	int positionWorth = 1;
+	long long int retVal = 0;
+	long long int positionWorth = 1;
 	while (val.length() > 0)
 	{
 		auto transChar = val.substr(val.length() - 1);
