c9c3d66379b05ffbc193c9a88e205fddbdd6dec0
RikardLundberg <rikardlundberg90@gmail.com>
2021-12-17 06:43:45 +0100

    day_17 first star

--- day_17/day_17.cpp
+++ day_17/day_17.cpp
@@ -1,20 +1,24 @@
-// day_17.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    int lowestY;
+    std::string input;
+    while (std::cin >> input && input != "eof")
+    {
+        if (input[0] == 'y')
+        {
+            std::string yVal = input.substr(input.find('=') + 1);
+            lowestY = std::stoi(yVal.substr(0, yVal.find('.')));
+        }
+    }
+
+    int height = 0;
+    int yMaxVelocity = lowestY * (-1) - 1;
+    for (int i = 0; i < yMaxVelocity; i++)
+        height += yMaxVelocity - i;
+
+    std::cout << "Max height is: " << height;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
